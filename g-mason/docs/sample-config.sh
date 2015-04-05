bb65703f4cb56ec2375bcf91e3372bdbf6d2df61
minhhn2910 <minhhn2910@users.noreply.github.com>
2015-04-06 02:43:51 +0800

    Update sample-config.sh

--- g-mason/docs/sample-config.sh
+++ g-mason/docs/sample-config.sh
@@ -6,5 +6,5 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_HOME/lib64:$JCUDA
 
 export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk.x86_64
 export MASON_LIB=/home/user/g-mason/libraries
-export CLASSPATH=:/home/user:$JCUDA/jcublas-0.6.5.jar:$JCUDA/jcuda-0.6.5.jar:$JCUDA/jcurand-0.6.5.jar:$JCUDA/jcusparse-0.6.5.jar:/home/user/g-mason:$MASON_LIB/itext-1.2.jar:$MASON_LIB/jcommon-1.0.16.jar:$MASON_LIB/jfreechart-1.0.13.jar:$MASON_LIB/jmf.jar:$MASON_LIB/portfolio.jar:/home/minh/jcuda
+export CLASSPATH=:/home/user:$JCUDA/jcublas-0.6.5.jar:$JCUDA/jcuda-0.6.5.jar:$JCUDA/jcurand-0.6.5.jar:$JCUDA/jcusparse-0.6.5.jar:/home/user/g-mason:$MASON_LIB/itext-1.2.jar:$MASON_LIB/jcommon-1.0.16.jar:$MASON_LIB/jfreechart-1.0.13.jar:$MASON_LIB/jmf.jar:$MASON_LIB/portfolio.jar:/home/user/jcuda
 export PATH=$JAVA_HOME/bin:$PATH
